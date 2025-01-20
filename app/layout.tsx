b4061b11b2f647fa6599de5c77493a3070ff4a66
Saunak Ghosh <saunakghosh710@gmail.com>
2025-01-20 19:50:31 +0530

    icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "FakeData - Generate Realistic Test Data",
   description: "Generate realistic, customizable fake data for your applications, testing, and development needs.",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
